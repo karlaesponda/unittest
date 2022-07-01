5fb092b952fcdd771bd67f3c12705005b766e37e
karlaesponda <kalejandra.kaec@gmail.com>
2022-07-01 17:43:34 -0500

    01/07/2022

---
+++ src/test/java/com/fca/calidad/unittest/TestGoogle.java
@@ -0,0 +1,113 @@
+package com.fca.calidad.unittest;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestGoogle {
+	  private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+	  JavascriptExecutor js;
+	@Before
+	public void setUp() throws Exception {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		baseUrl = "https://www.google.com/";
+	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	    js = (JavascriptExecutor) driver;
+	       
+	}
+
+	@Test
+	public void testUntitledTestCase() throws Exception {
+	    driver.get("https://www.google.com/");
+	    driver.findElement(By.name("q")).sendKeys(Keys.DOWN);
+	    driver.findElement(By.name("q")).clear();
+	    driver.findElement(By.name("q")).sendKeys("fotos gatitos");
+	    driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/a/h3")).click();
+	    driver.get("https://sp.depositphotos.com/stock-photos/gatito.html");
+	    assertEquals("Fotos de Gatito, Imágenes de Gatito ⬇ Descargar | Depositphotos", driver.getTitle());
+	    takeScreenshot("testBusqueda.jpg");
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+	  private void pause(long mils) {
+			try {
+				Thread.sleep(mils);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	  
+	  public void takeScreenshot(String name) {
+		    File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		    try {
+			    FileUtils.copyFile(scrFile, new File("src/screenshots/", name));
+		    }catch(IOException e) {
+		    	e.printStackTrace();
+		    }
+
+
+	  }
+}
